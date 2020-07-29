b50195804a59c2483454fb701c9d2f09b5269c97
matangyue@guomai.cc <971442841@qq.com>
2020-07-29 16:30:56 +0800

    webpack插件使用

--- demo2/webpack.config.js
+++ demo2/webpack.config.js
@@ -1,5 +1,23 @@
-const path = require('path')
-
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
- 
-}
+  mode: "development",
+  entry: { main: "./src/main.js", print: "./src/print.js" },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({
+      title: "webpack demo2",
+      template: "./index.html",
+    }),
+  ],
+};
