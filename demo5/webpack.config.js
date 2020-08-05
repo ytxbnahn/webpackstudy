f0cb64e8c513814eab6e755558ddcd4bce00481d
matangyue@guomai.cc <971442841@qq.com>
2020-08-05 18:08:39 +0800

    webpack 学习 library

--- demo5/webpack.config.js
+++ demo5/webpack.config.js
@@ -1,9 +1,34 @@
 const path = require('path');
 
+// module.exports = [
+//     'source-map'
+//   ].map(devtool => ({
+//     mode: 'development',
+//     entry: './src/index.js',
+//     output: {
+//       path: path.resolve(__dirname, 'dist'),
+//       filename: 'webpack-numbers.js'
+//     },
+//     devtool,
+//     optimization: {
+//       runtimeChunk: true
+//     }
+//   }));
+
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-  },
-};
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'webpack-numbers.js',
+      library: 'webpackNumbers',
+     libraryTarget: 'umd',
+    },
+    externals: {
+      lodash: {
+        commonjs: 'lodash',
+        commonjs2: 'lodash',
+        amd: 'lodash',
+        root: '_',
+      },
+    },
+  };
